9b427f8d3880e9701b7bc1e83ee50b9c35961cff
Anirudh Hegde <hegdeanirudh2003@gmail.com>
2024-05-17 22:34:02 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,5 +9,6 @@ def test_app_error():
     app_script_path=os.path.join(test_dir,"..","welcome_👋️.py")
     assert os.path.isfile(app_script_path),f"File not found at {app_script_path}"
     at = AppTest.from_file(app_script_path)
-    at.button[0].click().run()
+    # at.button[0].click().run()
+    st.sidebar[0].success().run()
     at.run()
