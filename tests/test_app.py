5faf94de58ec92b8b67fec4a2449f92ecafff755
Anirudh Hegde <hegdeanirudh2003@gmail.com>
2024-05-17 22:27:00 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import os.path
+
+from streamlit.testing.v1 import AppTest
+
+
+def test_app_error():
+    """tests any error occurs during testing the code of streamlit app"""
+    test_dir=os.path.dirname(os.path.abspath(__file__))
+    app_script_path=os.path.join(test_dir,"..","welcome_👋️.py")
+    assert os.path.isfile(app_script_path),f"File not found at {app_script_path}"
+    at = AppTest.from_file(app_script_path)
+    at.run()
